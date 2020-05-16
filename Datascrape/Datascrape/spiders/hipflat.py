0702380c6c04a81bf343f5edeb3de8697f7c4cf2
poomkhor <poom.khor@gmail.com>
2020-05-16 16:41:23 +0700

    fixed bug and change address to district >> find best ways to store data for later analysis

--- Datascrape/Datascrape/spiders/hipflat.py
+++ Datascrape/Datascrape/spiders/hipflat.py
@@ -14,20 +14,21 @@ class HipflatSpider(scrapy.Spider):
         # district_names = response.css('.directories__lists-element-name::text').getall()
         for district in districts:
             district_url = response.urljoin(district)    
-        yield Request(district_url, callback=self.parse_condo)
+            yield Request(district_url, callback=self.parse_condo)
     
     def parse_condo(self, response):
         condos = response.css('.directories__lists-element-name::attr(href)').getall() 
         for condo in condos:
             url = response.urljoin(condo)
-        yield Request(url, callback=self.parse_rental)
+            yield Request(url, callback=self.parse_rental)
 
     def parse_rental(self, response):
         yield {
-            'condo' : response.css('.project-header-name a::text').getall(),
+            'condo' : response.css('.project-header-name a::text').get(),
             'address' : response.css('span[itemprop=streetAddress]::text').get(),
-            'rental_rate' : response.css("div[id=listings-for-rent] .listing-row__primary .money .number::text").getall(),
-            'condo_area': response.css("div[id=listings-for-rent] .listing-row__cell-area span[data-type=area] .number::text").getall() 
-        }
-        print('Done')
+            'room_area': response.css("div[id=listings-for-rent] .listing-row__cell-area span[data-type=area] .number::text").getall(),
+            'rental_rate' : response.css("div[id=listings-for-rent] .listing-row__primary .money .number::text").getall()
+        }   
+
+
     

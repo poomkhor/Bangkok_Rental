6bedd38e57a40743bbdbef9916861b97677e7d5f
poomkhor <poom.khor@gmail.com>
2020-05-23 14:19:17 +0700

    rearrange the output layout >> not looping properly, repeat the same data then move to next loop

--- Datascrape/Datascrape/spiders/hipflat.py
+++ Datascrape/Datascrape/spiders/hipflat.py
@@ -1,15 +1,21 @@
 # -*- coding: utf-8 -*-
 import scrapy
-from scrapy import Request
+import re
+from scrapy import Request 
 
-# things to get rental price, sqm, type
-# 
+def cleanup(input_string):
+    if input_string:
+        return re.sub(r'[\n]','',input_string).strip()
+
 class HipflatSpider(scrapy.Spider):
     name = 'hipflat'
     start_urls = ['https://www.hipflat.co.th/en/market/condo-bangkok-skik']
 
-    
     def parse(self, response):
+        # district = response.css('.directories__lists-element-name::attr(href)').get()
+        # district_url = response.urljoin(district)    
+        # yield Request(district_url, callback=self.parse_condo)
+    
         districts = response.css('.directories__lists-element-name::attr(href)').getall()
         # district_names = response.css('.directories__lists-element-name::text').getall()
         for district in districts:
@@ -17,18 +23,40 @@ class HipflatSpider(scrapy.Spider):
             yield Request(district_url, callback=self.parse_condo)
     
     def parse_condo(self, response):
+        # condo = response.css('.directories__lists-element-name::attr(href)').get()
+        # url = response.urljoin(condo)
+        # yield Request(url, callback=self.parse_rental)
+
         condos = response.css('.directories__lists-element-name::attr(href)').getall() 
         for condo in condos:
             url = response.urljoin(condo)
             yield Request(url, callback=self.parse_rental)
 
     def parse_rental(self, response):
-        yield {
-            'condo' : response.css('.project-header-name a::text').get(),
-            'address' : response.css('span[itemprop=streetAddress]::text').get(),
-            'room_area': response.css("div[id=listings-for-rent] .listing-row__cell-area span[data-type=area] .number::text").getall(),
-            'rental_rate' : response.css("div[id=listings-for-rent] .listing-row__primary .money .number::text").getall()
-        }   
+        condo_name = response.css('.project-header-name a::text').get()
+        district = response.css('.breadcrumb li:nth-child(2) span::text').get()
+        # rooms = response.css("div[id=listings-for-rent] .listing-row__cell-area span[data-type=area] .number::text").getall()
+        for room in response.css('tbody > tr'):
+            rental_rate = response.css("div[id=listings-for-rent] .listing-row__primary .money .number::text").get()
+            if rental_rate is None:
+                continue
+
+            items = {
+                'condo' : condo_name,
+                'district' : cleanup(district),
+                'room_area' : response.css('td:nth-child(6) > span > span::text').get(),
+                'rental_rate' : response.css('td:nth-child(2) > div:nth-child(2) > div > span > span:nth-child(3)::text').get()
+                # 'room_area' : response.css("div[id=listings-for-rent] .listing-row__cell-area span[data-type=area] .number::text").get(),
+                # 'rental_rate' : response.css("div[id=listings-for-rent] .listing-row__primary .money .number::text").get()
+            }
+            
+            yield items
+        # item = {
+        #     'condo' : response.css('.project-header-name a::text').get(),
+        #     'district' : response.css('.breadcrumb li:nth-child(2) span::text').get(),
+        #     'room_area': response.css("div[id=listings-for-rent] .listing-row__cell-area span[data-type=area] .number::text").getall(),
+        #     'rental_rate' : response.css("div[id=listings-for-rent] .listing-row__primary .money .number::text").getall()
+        # }   
 
 
     
